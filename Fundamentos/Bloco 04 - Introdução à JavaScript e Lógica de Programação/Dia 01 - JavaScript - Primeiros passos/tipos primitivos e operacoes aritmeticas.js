f50335c94091b8f1d17e7e6bce99eb758849d168
wes-santos <weslleyalmerindo2@gmail.com>
2021-11-05 20:05:23 -0300

    Exercicios do dia 01 bloco 04

---
+++ Fundamentos/Bloco 04 - Introdução à JavaScript e Lógica de Programação/Dia 01 - JavaScript - Primeiros passos/tipos primitivos e operacoes aritmeticas.js
@@ -0,0 +1,18 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId, isEnrolled, patientInfo, patientEmail);
+console.log(typeof patientAge);
+
+const base = 5;
+let height = 8;
+const area = base * height;
+const perimeter = 2 * (base + height);
+
+console.log(area);
+console.log(perimeter);
---
+++ Fundamentos/Bloco 04 - Introdução à JavaScript e Lógica de Programação/Dia 01 - JavaScript - Primeiros passos/variaveis.js
@@ -0,0 +1,8 @@
+const myName = "Weslley";
+const birthCity = "Guarulhos";
+let birthYear = 1999;
+birthYear = 2030;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
