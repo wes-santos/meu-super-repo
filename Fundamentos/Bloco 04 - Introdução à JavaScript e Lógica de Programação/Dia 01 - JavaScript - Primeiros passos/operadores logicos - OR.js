8337cf19e628ecf852fa1e8a29781eebead5b279
wes-santos <weslleyalmerindo2@gmail.com>
2021-11-07 16:18:52 -0300

    adiciona exercicios de operadores logicos

---
+++ Fundamentos/Bloco 04 - Introdução à JavaScript e Lógica de Programação/Dia 01 - JavaScript - Primeiros passos/operadores logicos - OR.js
@@ -0,0 +1,7 @@
+let weekDay = "quarta-feira";
+
+if (weekDay === "segunda-feira" || "terça-feira" || "quarta-feira" || "quinta-feira" || "sexta-feira"){
+    console.log("Oba, mais um dia de aprendizado na Trybe :D");
+} else {
+    console.log("FINALMENTE, descanso merecido UwU");
+}
