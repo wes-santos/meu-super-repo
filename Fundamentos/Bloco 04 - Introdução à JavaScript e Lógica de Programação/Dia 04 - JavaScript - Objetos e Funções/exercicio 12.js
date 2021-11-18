68e7afec2ad208f8fa175a3a19bb4e7ca5218d05
wes-santos <weslleyalmerindo2@gmail.com>
2021-11-18 19:59:57 -0300

    atualiza exercicio12

--- Fundamentos/Bloco 04 - Introdução à JavaScript e Lógica de Programação/Dia 04 - JavaScript - Objetos e Funções/exercicio 12.js
+++ Fundamentos/Bloco 04 - Introdução à JavaScript e Lógica de Programação/Dia 04 - JavaScript - Objetos e Funções/exercicio 12.js
@@ -1,7 +1,13 @@
 let nomes = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
 
 function maiorNome (nomes) {
-    console.log(nomes);
+    let storage = [];
+    for (let i = 0; i < nomes.length; i += 1) {
+        storage.push(nomes[i].split(''));
+    }
+    return storage;
 }
-
-maiorNome(nomes);
+for (let i = 0; i < storage.length; i += 1) {
+    if (storage[i] < 
+}
+console.log(maiorNome(nomes));
