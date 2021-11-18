331f5337a6a1d4db18a8eb67da4e5df5bbd1b97d
wes-santos <weslleyalmerindo2@gmail.com>
2021-11-18 13:59:01 -0300

    tentativa de resolucao exercicio

--- Fundamentos/Bloco 05 - JavaScript: DOM, Eventos e Web Storage/Dia 02 - JavaScript - Trabalhando com elementos/exercicio.js
+++ Fundamentos/Bloco 05 - JavaScript: DOM, Eventos e Web Storage/Dia 02 - JavaScript - Trabalhando com elementos/exercicio.js
@@ -17,7 +17,26 @@ document.getElementById('filhoDoPrimeiroFilhoDoFilho').parentNode.parentNode.nex
 
 let start = document.getElementById('paiDoPai').firstElementChild;
 
-console.log(start.children);
+
+
+let removeElements = {
+    removePrimeroFilho: start.removeChild(start.children[0]),
+    removeTerceiroFilho: start.removeChild(start.children[1]),
+    removeQuartoEUltimoFilho: start.removeChild(start.children[1]),
+    removeSection: start.removeChild(start.children[1]),
+    removeText1: start.children[0].removeChild(start.children[0].childNodes[0]),
+    removeText2: start.children[0].removeChild(start.children[0].childNodes[1]),
+    removeSegundoEUltimoFilhoDoFilho: start.children[0].removeChild(start.children[0].childNodes[1]),
+    removeText3: start.children[0].removeChild(start.children[0].childNodes[1]),
+    removeDiv: start.children[0].removeChild(start.children[0].childNodes[1]),
+    removeText4: start.children[0].firstElementChild.innerText = "",
+    // removeText5: start.nextSibling.innerText = "",
+}
+
+console.log(start.childNodes[3].innerText = "");
+
+// Elementos para remover: primeiroFilho, segundoEUltimoFilhoDoFilho, terceiroFilho e quartoEUltimoFilho
+
 // start.removeChild(document.getElementById('primeiroFilho'));
 // start.removeChild(document.getElementById('segundoEUltimoFilhoDoFilho'));
 
